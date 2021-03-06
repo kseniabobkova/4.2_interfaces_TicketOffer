7d1873427b6c6ccc67078bd6e8fc9ad7970ca5cb
kseniabobkova <bobkova@healbe.com>
2021-03-06 13:41:54 +0300

    inicial commit

---
+++ src/main/java/ru/netology/domain/TicketOffer.java
@@ -0,0 +1,60 @@
+package ru.netology.domain;
+
+public class TicketOffer {
+    int id;
+    int cost;
+    String departureAirport;
+    String arrivalAirport;
+    int travelTime;
+
+    public TicketOffer() {
+    }
+
+    public TicketOffer(int id, int cost, String departureAirport, String arrivalAirport, int travelTime) {
+        this.id = id;
+        this.cost = cost;
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+        this.travelTime = travelTime;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public int getTravelTime() {
+        return travelTime;
+    }
+
+    public void setTravelTime(int travelTime) {
+        this.travelTime = travelTime;
+    }
+}
